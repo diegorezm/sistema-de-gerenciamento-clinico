d5cfee82673936b3984de194f85d098eaa9c7583
diegorezm <dmourarezende@gmail.com>
2025-09-25 22:50:37 -0300

    better home panel

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,89 +1,34 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
-import { ref } from "vue"
-import DoctorImage from "../../images/doctor.jpg"
-import Logo from "../../images/logo.svg"
-
-const links = ref([
-    {
-        title: "Principal",
-        href: "/"
-    },
-    {
-        title: "Sobre",
-        href: "/#sobre"
-    },
-    {
-        title: "Funcionalidades",
-        href: "/#funcionalidades"
-    },
-    {
-        title: "Planos",
-        href: "/#planos"
-    },
-    {
-        title: "Contato",
-        href: "/#contato"
-    },
-])
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import DoctorImage from '../../images/doctor.jpg';
 </script>
 
 <template>
-
     <Head title="Bem-vindo!">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <header class="bg-white px-4 py-2 shadow">
-        <nav class="flex justify-between items-center">
-            <div>
-                <a href="/">
-                    <img :src="Logo" class="size-9" alt="SGC">
-                </a>
-            </div>
-            <ul class="flex gap-1">
-                <li v-for="link in links" :key="link.title">
-                    <Link href="link.href" class="btn btn-sm btn-ghost">
-                    {{ link.title }}
-                    </Link>
-                </li>
-            </ul>
-            <div class="space-x-1">
-                <a href="/login" class="btn btn-sm btn-primary">
-                    Entrar
-                </a>
-                <a href="/register" class="btn btn-sm btn-outline">
-                    Registrar
-                </a>
-            </div>
-        </nav>
-    </header>
+    <AppLayout>
+        <main class="mt-4 w-full">
+            <div class="mx-auto flex max-w-7xl flex-col items-center md:flex-row">
+                <!-- Texto -->
+                <div class="flex-1 px-6 py-12 text-center md:px-12 md:text-left">
+                    <h1 class="mb-6 text-4xl leading-tight font-extrabold text-gray-900 md:text-5xl">A nova forma de <br />gerenciar sua clínica</h1>
+                    <p class="mb-8 text-lg text-gray-600">
+                        Organize consultas, pacientes e equipe em um só lugar. Simples, rápido e eficiente para o seu dia a dia.
+                    </p>
+                    <div class="flex flex-col justify-center gap-4 sm:flex-row md:justify-start">
+                        <a href="#agendar" class="btn btn-lg btn-primary"> Experimente grátis </a>
+                        <a href="#saiba-mais" class="btn btn-outline btn-lg"> Saiba mais </a>
+                    </div>
+                </div>
 
-    <section class="w-full  mt-4">
-        <div class="max-w-7xl mx-auto flex flex-col md:flex-row items-center">
-            <!-- Texto -->
-            <div class="flex-1 text-center md:text-left px-6 md:px-12 py-12">
-                <h1 class="text-4xl md:text-5xl font-extrabold text-gray-900 leading-tight mb-6">
-                    A nova forma de <br />gerenciar sua clínica
-                </h1>
-                <p class="text-lg text-gray-600 mb-8">
-                    Organize consultas, pacientes e equipe em um só lugar.
-                    Simples, rápido e eficiente para o seu dia a dia.
-                </p>
-                <div class="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
-                    <a href="#agendar" class="btn btn-lg btn-primary">
-                        Experimente grátis
-                    </a>
-                    <a href="#saiba-mais" class="btn btn-outline btn-lg">
-                        Saiba mais
-                    </a>
+                <!-- Imagem -->
+                <div class="flex-1">
+                    <img :src="DoctorImage" alt="Médico" class="h-[650px] w-full object-cover object-center" />
                 </div>
             </div>
-
-            <!-- Imagem -->
-            <div class="flex-1">
-                <img :src="DoctorImage" alt="Médico" class="w-full h-[650px] object-cover object-center" />
-            </div>
-        </div>
-    </section>
+        </main>
+    </AppLayout>
 </template>
