7066c162f512fe0cf6efbd82ac56dd1593ab841c
diegorezm <dmourarezende@gmail.com>
2025-09-25 22:00:16 -0300

    auth controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AuthController extends Controller
+{
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    /**
+     * Show login page
+     */
+    public function showLogin()
+    {
+        return Inertia::render('Auth/Login');
+    }
+
+    /**
+     * Handle login form submission
+     */
+    public function login(LoginRequest $request)
+    {
+        if ($this->authService->login($request->validated())) {
+            return redirect()
+                ->route('dashboard')
+                ->with('success', 'Bem-vindo de volta!');
+        }
+
+        return back()->withErrors([
+            'email' => 'As credenciais estão incorretas.',
+        ]);
+    }
+
+    /**
+     * Show registration page
+     */
+    public function showRegister()
+    {
+        return Inertia::render('Auth/Register');
+    }
+
+    /**
+     * Handle registration form submission
+     */
+    public function register(RegisterRequest $request)
+    {
+        $validatedRequest = $request->validated();
+        $user = $this->authService->register($validatedRequest);
+        $this->authService->login(["email" => $user->email, "password" => $validatedRequest['password']]);
+
+        return redirect()
+            ->route('dashboard')
+            ->with('success', 'Conta criada com sucesso!');
+    }
+
+    /**
+     * Logout user
+     */
+    public function logout(Request $request)
+    {
+        $this->authService->logout($request);
+
+        return redirect()
+            ->route('login')
+            ->with('success', 'Você saiu da sua conta.');
+    }
+}
