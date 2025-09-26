90b9753b72e9fdcf3e62e47a253b6b90113c2203
diegorezm <dmourarezende@gmail.com>
2025-09-25 22:50:25 -0300

    better navbar + added applayout

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Logo from '../../images/logo.svg';
+
+const links = ref([
+    {
+        title: 'Principal',
+        href: '/',
+    },
+    {
+        title: 'Sobre',
+        href: '/#sobre',
+    },
+    {
+        title: 'Funcionalidades',
+        href: '/#funcionalidades',
+    },
+    {
+        title: 'Planos',
+        href: '/#planos',
+    },
+    {
+        title: 'Contato',
+        href: '/#contato',
+    },
+]);
+</script>
+
+<template>
+    <header class="bg-white px-4 py-2 shadow">
+        <nav class="flex items-center justify-between">
+            <div>
+                <a href="/">
+                    <img :src="Logo" class="size-9" alt="SGC" />
+                </a>
+            </div>
+            <ul class="flex gap-1">
+                <li v-for="link in links" :key="link.title">
+                    <Link href="link.href" class="btn btn-sm btn-ghost">
+                        {{ link.title }}
+                    </Link>
+                </li>
+            </ul>
+            <div class="space-x-1">
+                <a href="/login" class="btn btn-sm btn-primary"> Entrar </a>
+                <a href="/register" class="btn btn-sm btn-outline"> Cadastrar </a>
+            </div>
+        </nav>
+    </header>
+</template>
---
+++ resources/js/layouts/AppLayout.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+import { usePage } from '@inertiajs/vue3';
+import { computed, watch } from 'vue';
+
+const page = usePage();
+const flash = computed(() => page.props.flash ?? {});
+
+watch(
+    () => flash.value,
+    (newVal) => {
+        if (newVal?.success) {
+            console.log('✅ SUCCESS:', newVal.success);
+            // here you could call a toast lib like vue-toastification
+        }
+        if (newVal?.error) {
+            console.error('❌ ERROR:', newVal.error);
+        }
+    },
+    { deep: true },
+);
+</script>
+
+<template>
+    <Navbar />
+    <div>
+        <div v-if="flash.success" class="mb-2 bg-green-100 p-2 text-green-800">
+            {{ flash.success }}
+        </div>
+
+        <div v-if="flash.error" class="mb-2 bg-red-100 p-2 text-red-800">
+            {{ flash.error }}
+        </div>
+
+        <slot />
+    </div>
+</template>
